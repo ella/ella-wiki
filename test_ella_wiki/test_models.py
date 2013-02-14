7e066a53d4f8e87cf41450e6641118ef3e8d1410
Honza Kral <honza.kral@gmail.com>
2013-02-14 02:46:28 +0100

    smallest of refactors

--- test_ella_wiki/test_models.py
+++ test_ella_wiki/test_models.py
@@ -10,21 +10,22 @@ class TestWikiModel(TestCase):
     def setUp(self):
         super(TestWikiModel, self).setUp()
         create_basic_categories(self)
+        self.wiki = create_wiki(self, slug='first-article')
 
     def test_tree_path(self):
-        wiki = create_wiki(self, slug='first-article')
-        wiki2 = create_wiki(self, tree_parent=wiki, slug='second')
+        wiki2 = create_wiki(self, tree_parent=self.wiki, slug='second')
 
-        tools.assert_equals('first-article', wiki.tree_path)
+        tools.assert_equals('first-article', self.wiki.tree_path)
         tools.assert_equals('first-article/second', wiki2.tree_path)
 
-        tools.assert_equals('/wiki/first-article/', wiki.get_absolute_url())
+        tools.assert_equals('/wiki/first-article/', self.wiki.get_absolute_url())
         tools.assert_equals('/wiki/first-article/second/', wiki2.get_absolute_url())
 
     def test_wiki_with_no_submission_is_unpublished(self):
-        wiki = create_wiki(self, slug='first-article')
-        tools.assert_false(wiki.is_published())
+        tools.assert_false(self.wiki.is_published())
 
     def test_wiki_with_submission_is_published(self):
-        submission = create_submission(self)
+        submission = create_submission(self, wiki=self.wiki)
         tools.assert_true(submission.wiki.is_published())
+
+
